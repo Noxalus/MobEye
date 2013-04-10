5e1c2b9e2088376d2b13b776acb9960b8ed46c69
chevri_t <thibault.chevrin@epita.fr>
2013-04-10 17:56:31 +0200

    tests urls

--- api.php
+++ api.php
@@ -118,7 +118,7 @@ function new_user($email_password_first_name_last_name_birth_date_place)
 
 function get_missions()
 {
-	include 'functions';
+	include 'functions.php';
 	
 	$result = GetMissions();
 	
