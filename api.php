d17db4843099c55a9e2d468d8c4ecd6a53f1026c
chevri_t <thibault.chevrin@epita.fr>
2013-04-10 14:06:19 +0200

    Api functions

---
+++ api.php
@@ -0,0 +1,38 @@
+<?php
+
+function login($email, $password)
+{
+}
+
+function is_login($cookie)
+{
+
+}
+
+function logout($cookie)
+{
+}
+
+function new_user($email, $password, $first_name, $last_name, $birth_date, $place)
+{
+}
+
+function get_missions()
+{
+}
+
+function get_user($cookie)
+{
+}
+
+function accept_mission($cookie, $id_mission)
+{
+}
+
+function send_data($mission_id, $mission_text, $pictures)
+{
+}
+
+function miss_mission($cookie, $id_mission)
+{
+}
